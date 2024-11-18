f43c6de759d9d7009d3bda090d4c558022de1a83
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2024-11-18 17:36:50 +0100

    PowerBi script genaration: find all sources

--- source/create_power_bi_script.R
+++ source/create_power_bi_script.R
@@ -1,46 +1,124 @@
 library(tidyverse)
 library(rebus)
+source("source/functions.R")
+print(clc)
 rm(list = ls())
 
-find_connected_source <- function(c_file){
-  c_raw <- readLines(c_file)
+find_connected_source <- function(c_path, c_file){
+  
+  if(c_path == ""){
+    c_raw <- paste0(c_path, c_file)|>
+      readLines()  
+  }else{
+    c_raw <- paste0(c_path, "/",c_file)|>
+      readLines()
+  }
+  
+  c_raw
   # Ändern des Templates mit user eingaben (ii <- ??) verwendet für Datum
-  index <- (1:length(c_raw))[c_raw|>str_detect("source")]
-  index
+  df_index <- tibble(index = (1:length(c_raw))[c_raw|>str_detect("source")])
   
-  c_temp <- str_split(c_raw[index],"/", simplify = T)
-  c_temp
+  if(nrow(df_index)>0){
+    df_index <- df_index|>
+      mutate(raw = c_raw[index])|>
+      separate(raw, into = c("path", "file_"), sep = "/", remove = T )|>
+      filter(path != "",
+             path != c_path,
+             !str_detect(path ,"#"),
+             !str_detect(file_, DOT%R%"Rmd"))|>
+      mutate(path = str_remove(path,"source")|>
+               str_remove(literal("("))|>
+               str_remove(literal("\"")),
+             file_ = file_|>
+               str_remove(literal(")"))|>
+               str_remove(literal("\""))
+             )|>
+      suppressWarnings()
+    df_index
+    
+    df_index <- df_index|>
+      mutate(link = if_else(c_path != "",
+                            c(c_path, c_file)|>
+                              paste0(collapse = "/"),
+                            c_file
+                            ),
+             checked = F)
+    df_index <- df_index|>
+      mutate(filePath = if_else(path == "",
+                                paste0(path, "/", file_),
+                                file_
+                                )|>
+               str_trim(),
+             path = str_trim(path)
+      )
+    return(df_index)
+  }else{
+    return(NULL)
+  }
+}
+c_path <- "source"
+c_path <- ""
+c_file <- "calculate.R"
+c_file <- "read and convert.R"
+c_file <- "Erstelle Abrechnung.R"
+find_connected_source(c_path, c_file)
+
+##############################################################################################################
+# find all connected source
+##############################################################################################################
+df_source <- tibble(path = c_path,
+                    file_ = c_file,
+                    filePath = paste0(c_path, c_file),
+                    index = 1, 
+                    checked = F,
+                    link = "",
+                    level = 1L
+                    )
+df_source
+
+cnt <- 1L
+l_source <- list()
+while (TRUE) {
+  paste0("\n*****************************  ",cnt,"  *****************************\n")|>writeLines()
+  df_source
+  df_temp <- df_source|>
+    filter(!checked)|>
+    mutate(level = cnt)
+  df_temp
+
+  if(nrow(df_temp) > 0){
+    l_temp <- list()
+    df_temp$checked[1] <- TRUE
+    for (ii in 1:nrow(df_temp)) {
+      # paste0("\nfor loop: ", ii )|>
+      #   writeLines()
+      if(!is.null(find_connected_source(df_temp$path[ii], df_temp$file_[ii]))){
+        l_temp[[ii]] <- 
+          find_connected_source(df_temp$path[ii], df_temp$file_[ii])|>
+          mutate(level = cnt)
+        # paste0("\nFound new source loop ii: ", ii )|>
+        #   writeLines()
+      }
+    }
+  }else{
+    break
+  }
+  df_source <- 
+    bind_rows(df_temp|>slice(1),
+              l_temp|>bind_rows())
   
-  c_file <- c_temp[,2]|>
-    str_remove_all(literal(")"))|>
-    str_remove_all(literal("\""))
-  c_file
-  
-  c_path <- str_split(c_temp[,1], literal("("), simplify = T)[,1]|>
-    paste0("/")
-  c_path
-  
-  tibble(path = c_path,
-         file = c_file, 
-         filePath = paste0(c_path, c_file),
-         index, 
-         checked = F
-         )|>
-    filter(!str_detect(file, DOT%R%"Rmd"),
-           !str_detect(path, "#"))|>
-    mutate(path = str_trim(path),
-           file = str_trim(file),
-           filePath = str_trim(filePath))
+  l_source[[cnt]] <- df_source
+  # print(df_source)  
+  cnt <- cnt + 1
 }
 
-c_file <- "source/calculate.R"
-find_connected_source(c_file)|>
-  print()
-
-
-c_file <- "Erstelle Abrechnung.R"
-find_connected_source(c_file)|>
+df_source <- l_source|>
+  bind_rows()
+df_source|>
   print()
 
 
 ##############################################################################################################
+# stitch together all connected sources
+##############################################################################################################
+
