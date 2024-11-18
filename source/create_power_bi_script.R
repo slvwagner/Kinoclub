41ffbeeed923f4caaa8236f4358027a65a6aec3b
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2024-11-18 19:37:20 +0100

    mc

--- source/create_power_bi_script.R
+++ source/create_power_bi_script.R
@@ -4,8 +4,7 @@ source("source/functions.R")
 print(clc)
 rm(list = ls())
 
-find_connected_source <- function(c_path, c_file){
-  
+read_source <- function(c_path, c_file){
   if(c_path == ""){
     c_raw <- paste0(c_path, c_file)|>
       readLines()  
@@ -13,10 +12,16 @@ find_connected_source <- function(c_path, c_file){
     c_raw <- paste0(c_path, "/",c_file)|>
       readLines()
   }
-  
-  c_raw
-  # Ändern des Templates mit user eingaben (ii <- ??) verwendet für Datum
-  df_index <- tibble(index = (1:length(c_raw))[c_raw|>str_detect("source")])
+  return(c_raw)
+}
+
+check_source <- function(c_raw){
+  tibble(index = (1:length(c_raw))[c_raw|>str_detect("source")])
+}
+
+find_connected_source <- function(c_path, c_file){
+  c_raw <- read_source(c_path, c_file)
+  df_index <- check_source(c_raw)
   
   if(nrow(df_index)>0){
     df_index <- df_index|>
@@ -45,8 +50,8 @@ find_connected_source <- function(c_path, c_file){
              checked = F)
     df_index <- df_index|>
       mutate(filePath = if_else(path == "",
-                                paste0(path, "/", file_),
-                                file_
+                                paste0(path, file_),
+                                paste0(path, "/",file_)
                                 )|>
                str_trim(),
              path = str_trim(path)
@@ -57,8 +62,8 @@ find_connected_source <- function(c_path, c_file){
   }
 }
 c_path <- "source"
-c_path <- ""
 c_file <- "calculate.R"
+c_path <- ""
 c_file <- "read and convert.R"
 c_file <- "Erstelle Abrechnung.R"
 find_connected_source(c_path, c_file)
@@ -68,7 +73,7 @@ find_connected_source(c_path, c_file)
 ##############################################################################################################
 df_source <- tibble(path = c_path,
                     file_ = c_file,
-                    filePath = paste0(c_path, c_file),
+                    filePath = paste0(c_path,"/", c_file),
                     index = 1, 
                     checked = F,
                     link = "",
@@ -79,7 +84,7 @@ df_source
 cnt <- 1L
 l_source <- list()
 while (TRUE) {
-  paste0("\n*****************************  ",cnt,"  *****************************\n")|>writeLines()
+  # paste0("\n*****************************  ",cnt,"  *****************************\n")|>writeLines()
   df_source
   df_temp <- df_source|>
     filter(!checked)|>
@@ -107,18 +112,48 @@ while (TRUE) {
     bind_rows(df_temp|>slice(1),
               l_temp|>bind_rows())
   
-  l_source[[cnt]] <- df_source
+  l_source[[cnt]] <- df_source|>
+    mutate(checked = T)
   # print(df_source)  
   cnt <- cnt + 1
 }
 
 df_source <- l_source|>
-  bind_rows()
-df_source|>
-  print()
+  bind_rows()|>
+  mutate(filePath = str_remove(filePath, START%R%literal("/")),
+         index = row_number())
+# df_source|>
+#   print()
 
 
 ##############################################################################################################
 # stitch together all connected sources
 ##############################################################################################################
+
+# read all sources
+l_raw <- df_source$filePath|>
+  lapply(readLines)
+ii <- 16
+for (ii in sort(df_source$index, decreasing = T)) {
+  df_temp <- df_source|>
+    filter(index == ii)
+  df_temp
+
+  if(df_temp$link == "") next
+  
+  l_raw <- df_temp$link|>
+    lapply(readLines)
+  
+  for (jj in 1:length(l_raw)) {
+    df_index <- l_raw[[jj]]|>
+      check_source()
+    df_index|>
+      mutate(c_source = "df")
+    
+  }
+  
+}
+
+writeLines("\nPowerBi script generated")
+
 
