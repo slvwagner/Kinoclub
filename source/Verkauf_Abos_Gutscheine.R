e3f1b6a929959c58c52c857a1c73c56303402a0a
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-01-11 17:20:51 +0100

    remove unused code

--- source/Verkauf_Abos_Gutscheine.R
+++ source/Verkauf_Abos_Gutscheine.R
@@ -1,16 +1,15 @@
-## read in all kiosk files
-
 library(rebus)
 library(tidyverse)
 library(lubridate)
 library(readxl)
-source("source/functions.R")
+library(readr)
 
+source("source/functions.R")
 
 ######################################################################## 
 # Read in all abos
 ######################################################################## 
-library(readr)
+if(!file.exists("Input/advance tickets/atelierkino_abo.txt")) stop("Die Datei: .../Input/advance tickets/atelierkino_abo.txt wurde nicht gefunden.")
 atelierkino_abo <- read_delim("Input/advance tickets/atelierkino_abo.txt", 
                               delim = "\t", escape_double = FALSE, 
                               col_types = cols(creation = col_date(format = "%Y-%m-%d"), 
@@ -19,7 +18,7 @@ atelierkino_abo <- read_delim("Input/advance tickets/atelierkino_abo.txt",
                                                expiration = col_date(format = "%Y-%m-%d"), 
                                                count_use = col_integer()), trim_ws = TRUE)
 
-
+if(!file.exists("Input/advance tickets/atelierkino_foerderer.txt")) stop("Die Datei: .../Input/advance tickets/atelierkino_foerderer.txt wurde nicht gefunden.")
 atelierkino_foerderer <- read_delim("Input/advance tickets/atelierkino_foerderer.txt", 
                                     delim = "\t", escape_double = FALSE, 
                                     col_types = cols(creation = col_date(format = "%Y-%m-%d"), 
@@ -28,7 +27,7 @@ atelierkino_foerderer <- read_delim("Input/advance tickets/atelierkino_foerderer
                                                      expiration = col_date(format = "%Y-%m-%d"), 
                                                      count_use = col_integer()), trim_ws = TRUE)
 
-
+if(!file.exists("Input/advance tickets/atelierkino_gutschein.txt")) stop("Die Datei: .../Input/advance tickets/atelierkino_gutschein.txt wurde nicht gefunden.")
 atelierkino_gutschein <- read_delim("Input/advance tickets/atelierkino_gutschein.txt", 
                                     delim = "\t", escape_double = FALSE, 
                                     col_types = cols(creation = col_date(format = "%Y-%m-%d"), 
@@ -37,186 +36,4 @@ atelierkino_gutschein <- read_delim("Input/advance tickets/atelierkino_gutschein
                                                      expiration = col_date(format = "%Y-%m-%d"), 
                                                      amount = col_double(), count_use = col_integer()), 
                                     trim_ws = TRUE)
-
-######################################################################## 
-# files to read in 
-######################################################################## 
-c_path <- "input/advance tickets/"
-c_files <- c(list.files(path = c_path,pattern = "Abo Gutscheine", recursive = T), list.files(path = c_path, pattern = "Kiosk", recursive = F) )
-c_files
-
-
-l_raw <- lapply(c_files, function (x) suppressWarnings(readLines(paste0(c_path,x))))
-l_raw
-
-## extract file date 
-p <- capture(one_or_more(DGT)%R%DOT%R%one_or_more(DGT)%R%DOT%R%one_or_more(DGT))%R%DOT%R%"txt"
-
-c_fileDate <- str_match(c_files, p)[,2]
-c_fileDate
-
-p <- rebus::or(rebus::optional("-")%R%one_or_more(DGT)%R%DOT%R%one_or_more(DGT),
-               rebus::optional("-")%R%one_or_more(DGT)
-               )
-
-######################################################################## 
-# Verkauf: Abo 
-########################################################################
-
-l_Abos <- l_raw|>
-  lapply(function(x){
-    y <- x[str_detect(x,START%R%"Verkauf Abo")]|>
-      str_split(pattern = "\t", simplify = T)
-    y
-    c_lenght <- ncol(y)
-    c_lenght
-    
-    if(c_lenght == 7){ # mit Korrekturbuchungen
-      if(nrow(y) == 1){ #matrix convertierung in vector verhindern ncol = 1
-        y <- y[,c(1:2,4:5,7)]|>
-          matrix(ncol = 5)
-        c_Verkaufsartikel <- y[,1]
-        y <- y[,2:ncol(y)]|>
-          lapply(as.numeric)|>
-          unlist()|>
-          matrix(ncol = 4)
-      }else{
-        y <- y[,c(1:2,4:5,7)]
-        c_Verkaufsartikel <- y[,1]
-        y <- y[,2:ncol(y)]|>
-          apply(2, as.numeric)
-      }
-      y
-      
-      colnames(y) <- c("Einzelpreis", "Anzahl", "Korrektur", "Betrag")
-      bind_cols(Verkaufsartikel = c_Verkaufsartikel, y)|>
-        mutate(Anzahl = if_else(is.na(Korrektur), Anzahl,Anzahl + Korrektur))|>
-        select(-Korrektur)
-      
-    }else if(c_lenght == 5){ # keine Korrekturbuchungen
-      y <- y[,c(1:3,5)]
-      c_Verkaufsartikel <- y[,1]
-      y <- y[,2:ncol(y)]|>
-        apply(2, as.numeric)
-      colnames(y) <- c("Einzelpreis", "Anzahl", "Betrag")
-      bind_cols(Verkaufsartikel = c_Verkaufsartikel, y)
-    }
-  })
-
-names(l_Abos) <- dmy(c_fileDate)
-l_Abos
-
-df_Abo_Verkauf <- bind_rows(l_Abos,.id = "Datum")|>
-  mutate(Datum = as.Date(Datum))
-
-df_Abo_Verkauf
-
-######################################################################## 
-# Verkauf: Kinogutschein
-######################################################################## 
-p <- rebus::or(rebus::optional("-")%R%one_or_more(DGT)%R%DOT%R%one_or_more(DGT),
-               rebus::optional("-")%R%one_or_more(DGT)
-)
-
-l_Abos <- l_raw|>
-  lapply(function(x){
-    y <- x[str_detect(x,START%R%"Verkauf Kinogutschein")]|>
-      str_split(pattern = "\t", simplify = T)
-    y
-    c_lenght <- ncol(y)
-    c_lenght
-    
-    if(c_lenght == 7){ # mit Korrekturbuchungen
-      if(nrow(y) == 1){ #matrix convertierung in vector verhindern ncol = 1
-        y <- y[,c(1:2,4:5,7)]|>
-          matrix(ncol = 5)
-        c_Verkaufsartikel <- y[,1]
-        y <- y[,2:ncol(y)]|>
-          lapply(as.numeric)|>
-          unlist()|>
-          matrix(ncol = 4)
-      }else{
-        y <- y[,c(1:2,4:5,7)]
-        c_Verkaufsartikel <- y[,1]
-        y <- y[,2:ncol(y)]|>
-          apply(2, as.numeric)
-      }
-      y
-      
-      colnames(y) <- c("Einzelpreis", "Anzahl", "Korrektur", "Betrag")
-      bind_cols(Verkaufsartikel = c_Verkaufsartikel, y)|>
-        mutate(Anzahl = if_else(is.na(Korrektur), Anzahl,Anzahl + Korrektur))|>
-        select(-Korrektur)
-      
-    }else if(c_lenght == 5){ # keine Korrekturbuchungen
-      y <- y[,c(1:3,5)]
-      c_Verkaufsartikel <- y[,1]
-      y <- y[,2:ncol(y)]|>
-        apply(2, as.numeric)
-      colnames(y) <- c("Einzelpreis", "Anzahl", "Betrag")
-      bind_cols(Verkaufsartikel = c_Verkaufsartikel, y)
-    }
-  })
-
-names(l_Abos) <- dmy(c_fileDate)
-
-df_Kinogutschein_Verkauf <- bind_rows(l_Abos,.id = "Datum")|>
-  mutate(Datum = as.Date(Datum))
-  
-df_Kinogutschein_Verkauf
-
-
-######################################################################## 
-# Eingelöst: Kinogutschein
-######################################################################## 
-p <- rebus::or(rebus::optional("-")%R%one_or_more(DGT)%R%DOT%R%one_or_more(DGT),
-               rebus::optional("-")%R%one_or_more(DGT)
-)
-
-l_Abos <- l_raw|>
-  lapply(function(x){
-    y <- x[str_detect(x,START%R%"Kinogutschein")]|>
-      str_split(pattern = "\t", simplify = T)
-    y
-    c_lenght <- ncol(y)
-    c_lenght
-    
-    if(c_lenght == 7){ # mit Korrekturbuchungen
-      if(nrow(y) == 1){ #matrix convertierung in vector verhindern ncol = 1
-        y <- y[,c(1:2,4:5,7)]|>
-          matrix(ncol = 5)
-        c_Verkaufsartikel <- y[,1]
-        y <- y[,2:ncol(y)]|>
-          lapply(as.numeric)|>
-          unlist()|>
-          matrix(ncol = 4)
-      }else{
-        y <- y[,c(1:2,4:5,7)]
-        c_Verkaufsartikel <- y[,1]
-        y <- y[,2:ncol(y)]|>
-          apply(2, as.numeric)
-      }
-      y
-      
-      colnames(y) <- c("Einzelpreis", "Anzahl", "Korrektur", "Betrag")
-      bind_cols(Verkaufsartikel = c_Verkaufsartikel, y)|>
-        mutate(Anzahl = if_else(is.na(Korrektur), Anzahl,Anzahl + Korrektur))|>
-        select(-Korrektur)
-      
-    }else if(c_lenght == 5){ # keine Korrekturbuchungen
-      y <- y[,c(1:3,5)]
-      c_Verkaufsartikel <- y[,1]
-      y <- y[,2:ncol(y)]|>
-        apply(2, as.numeric)
-      colnames(y) <- c("Einzelpreis", "Anzahl", "Betrag")
-      bind_cols(Verkaufsartikel = c_Verkaufsartikel, y)
-    }
-  })
-
-names(l_Abos) <- dmy(c_fileDate)
-l_Abos
-
-df_Kinogutschein_Eingeloest <- bind_rows(l_Abos,.id = "Datum")|>
-  mutate(Datum = as.Date(Datum))|>
-  left_join(df_show, by = join_by(Datum))
 
